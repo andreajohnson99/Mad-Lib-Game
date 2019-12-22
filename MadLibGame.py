4564b05fce0569d1030f6a1add6fed11c04f6ace
Andrea Johnson <54482569+aztechvegan@users.noreply.github.com>
2019-12-22 15:56:35 -0500

    Create MadLibGame.py

---
+++ MadLibGame.py
@@ -0,0 +1,10 @@
+#Create three variables
+#and store the users inputs
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+#print results
+print("Roses are " + color)
+print(plural_noun +" are blue")
+print("I love " + celebrity)
